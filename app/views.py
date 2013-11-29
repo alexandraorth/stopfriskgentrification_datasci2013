9eb52f64fab4624441951a35d51be02ca505121b
jennifer <jrubinovitz@gmail.com>
2013-11-29 17:22:50 -0500

    starting to port the graphs to the flask webapp and add styling

--- app/views.py
+++ app/views.py
@@ -1,9 +1,24 @@
 from app import app
 from flask import render_template, flash, redirect, session, url_for, request, g
+from werkzeug.wsgi import SharedDataMiddleware
+import os
 
 @app.route('/')
 @app.route('/index')
 def index():
 	url_for('static', filename='bootstrap/css/bootstrap.min.css')
 	return render_template('index.html')
+
+@app.route('/race-stops')
+def race_stops():
+	return render_template('race_stops.html')
+
+@app.route('/total-stops')
+def total_stops():
+	return render_template('total_stops_map.html')
+
+# store assets files on server for now
+app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+	'/': os.path.join(os.path.dirname(__file__), '/static')
+})
 
